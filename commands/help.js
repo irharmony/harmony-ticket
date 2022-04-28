9e3e4c2c3ee754f097b485f97cf3da7472d5d4fa
hesawmgh1098 <hesam.ghanbari1098@gmail.com>
2022-04-29 01:50:51 +0430

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'help',
 
     execute(client, message) {
-        message.channel.send(`To configure the ticket message **setup** and to get the bot latency **ping** ✅`);
+        message.channel.send(`برای کانفیگ چنل تیکت از دستور مقابل استفاده کنید **setup** و برای دریافت پینگ بات از دستور مقابل استفاده کنید**ping** ✅`);
     },
 };
