957d528fd18cdcda676dd310d12f1bf5d211c8dd
hadi <65118892+hadiazt@users.noreply.github.com>
2022-04-29 11:55:11 +0430

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,1 +1,16 @@
-module.exports = (client) => console.log(`${client.user.username} ready to manage tickets !`);
+const { Category } = require('../config.json')
+module.exports = (client) => {
+    console.log(`${client.user.username} ready to manage tickets !`);
+
+    setInterval(() => {
+        var COUNT = client.channels.cache.filter(c => c.type === 'GUILD_TEXT' && c.parentId === Category).size
+
+        client.user.setPresence({
+            status: 'dnd',
+            activities: [{
+                type: 'WATCHING',
+                name: COUNT + 'Open Tickets',
+            }]
+        });
+    }, 60000);
+}
