a656ed99874347b936abd361a2024606bac35cf9
hadi <65118892+hadiazt@users.noreply.github.com>
2022-04-29 15:22:26 +0430

    Update ticketsLogs.js

--- events/ticketsLogs.js
+++ events/ticketsLogs.js
@@ -2,63 +2,63 @@ const { MessageEmbed } = require('discord.js')
 const { Log } = require('../config.json')
 
 module.exports = (client, type, guild, user) => {
-const channel = client.guild.channels.cache.get(Log)
-const logEmbed = new MessageEmbed()
+    const channel = client.guild.channels.cache.get(Log)
+    const logEmbed = new MessageEmbed()
 
     switch (type) {
 
         case 'newTicket': {
 
-            .setColor('') 
-            .setTitle('')
-            .setAuthor({ name: '', iconURL: '', url: '' }) 
-            .setDescription('') 	
-            .setTimestamp() 	
-            .setFooter({ text: '', iconURL: '' });    
+            logEmbed.setColor('')
+            logEmbed.setTitle('')
+            logEmbed.setAuthor({ name: '', iconURL: '', url: '' })
+            logEmbed.setDescription('')
+            logEmbed.setTimestamp()
+            logEmbed.setFooter({ text: '', iconURL: '' });
             return console.log(`${user.username} just created a ticket on the server ${guild.name}`);
         }
 
         case 'closeTicket': {
 
-            .setColor('') 
-            .setTitle('')
-            .setAuthor({ name: '', iconURL: '', url: '' }) 
-            .setDescription('') 	
-            .setTimestamp() 	
-            .setFooter({ text: '', iconURL: '' });
+            logEmbed.setColor('')
+            logEmbed.setTitle('')
+            logEmbed.setAuthor({ name: '', iconURL: '', url: '' })
+            logEmbed.setDescription('')
+            logEmbed.setTimestamp()
+            logEmbed.setFooter({ text: '', iconURL: '' });
             return console.log(`${user.username} just closed a ticket on the server ${guild.name}`);
         }
 
         case 'reopenTicket': {
 
-            .setColor('') 
-            .setTitle('')
-            .setAuthor({ name: '', iconURL: '', url: '' }) 
-            .setDescription('') 	
-            .setTimestamp() 	
-            .setFooter({ text: '', iconURL: '' });
+            logEmbed.setColor('')
+            logEmbed.setTitle('')
+            logEmbed.setAuthor({ name: '', iconURL: '', url: '' })
+            logEmbed.setDescription('')
+            logEmbed.setTimestamp()
+            logEmbed.setFooter({ text: '', iconURL: '' });
             return console.log(`${user.username} just reopened a ticket on the server ${guild.name}`);
         }
 
         case 'deleteTicket': {
 
-            .setColor('') 
-            .setTitle('')
-            .setAuthor({ name: '', iconURL: '', url: '' }) 
-            .setDescription('') 	
-            .setTimestamp() 	
-            .setFooter({ text: '', iconURL: '' });
+            logEmbed.setColor('')
+            logEmbed.setTitle('')
+            logEmbed.setAuthor({ name: '', iconURL: '', url: '' })
+            logEmbed.setDescription('')
+            logEmbed.setTimestamp()
+            logEmbed.setFooter({ text: '', iconURL: '' });
             return console.log(`${user.username} just deleted a ticket on the server ${guild.name}`);
         }
 
         case 'saveTicket': {
 
-            .setColor('') 
-            .setTitle('')
-            .setAuthor({ name: '', iconURL: '', url: '' }) 
-            .setDescription('') 	
-            .setTimestamp() 	
-            .setFooter({ text: '', iconURL: '' });
+            logEmbed.setColor('')
+            logEmbed.setTitle('')
+            logEmbed.setAuthor({ name: '', iconURL: '', url: '' })
+            logEmbed.setDescription('')
+            logEmbed.setTimestamp()
+            logEmbed.setFooter({ text: '', iconURL: '' });
             return console.log(`${user.username} just saved a ticket on the server ${guild.name}`);
         }
     }
