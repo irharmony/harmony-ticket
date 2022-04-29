9bc83902f75eaf85a1b9be2692a0023d89547b9c
hesawmgh1098 <hesam.ghanbari1098@gmail.com>
2022-04-29 13:43:34 +0430

    Update ticketsLogs.js

--- events/ticketsLogs.js
+++ events/ticketsLogs.js
@@ -1,25 +1,61 @@
+const { MessageEmbed } = require('discord.js')
 const { Log } = require('../config.json)
 module.exports = (client, type, guild, user) => {
 const channel = client.guild.channels.cache.get(Log)
     switch (type) {
 
         case 'newTicket': {
+            const embed = new MessageEmbed()
+            .setColor('') 
+            .setTitle('')
+            .setAuthor({ name: '', iconURL: '', url: '' }) 
+            .setDescription('') 	
+            .setTimestamp() 	
+            .setFooter({ text: '', iconURL: '' });    
             return console.log(`${user.username} just created a ticket on the server ${guild.name}`);
         }
 
         case 'closeTicket': {
+            const embed = new MessageEmbed()
+            .setColor('') 
+            .setTitle('')
+            .setAuthor({ name: '', iconURL: '', url: '' }) 
+            .setDescription('') 	
+            .setTimestamp() 	
+            .setFooter({ text: '', iconURL: '' });
             return console.log(`${user.username} just closed a ticket on the server ${guild.name}`);
         }
 
         case 'reopenTicket': {
+            const embed = new MessageEmbed()
+            .setColor('') 
+            .setTitle('')
+            .setAuthor({ name: '', iconURL: '', url: '' }) 
+            .setDescription('') 	
+            .setTimestamp() 	
+            .setFooter({ text: '', iconURL: '' });
             return console.log(`${user.username} just reopened a ticket on the server ${guild.name}`);
         }
 
         case 'deleteTicket': {
+            const embed = new MessageEmbed()
+            .setColor('') 
+            .setTitle('')
+            .setAuthor({ name: '', iconURL: '', url: '' }) 
+            .setDescription('') 	
+            .setTimestamp() 	
+            .setFooter({ text: '', iconURL: '' });
             return console.log(`${user.username} just deleted a ticket on the server ${guild.name}`);
         }
 
         case 'saveTicket': {
+            const embed = new MessageEmbed()
+            .setColor('') 
+            .setTitle('')
+            .setAuthor({ name: '', iconURL: '', url: '' }) 
+            .setDescription('') 	
+            .setTimestamp() 	
+            .setFooter({ text: '', iconURL: '' });
             return console.log(`${user.username} just saved a ticket on the server ${guild.name}`);
         }
     }
