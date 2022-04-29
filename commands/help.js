1ea73b6c318840e830d3e9d00c0c97b55c0a1c4c
hadi <65118892+hadiazt@users.noreply.github.com>
2022-04-29 10:27:38 +0430

    Delete help.js

--- commands/help.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'help',
-
-    execute(client, message) {
-        message.channel.send(`برای کانفیگ چنل تیکت از دستور مقابل استفاده کنید **setup** و برای دریافت پینگ بات از دستور مقابل استفاده کنید**ping** ✅`);
-    },
-};
