07842a94c844f33c4cc034b88623ec4ab82753a0
hadi <65118892+hadiazt@users.noreply.github.com>
2022-04-29 15:45:13 +0430

    Update ticketsLogs.js

--- events/ticketsLogs.js
+++ events/ticketsLogs.js
@@ -2,65 +2,46 @@ const { MessageEmbed } = require('discord.js')
 const { Log } = require('../config.json')
 
 module.exports = (client, type, guild, user) => {
-    const channel = client.guild.channels.cache.get(Log)
+    const channel = client.channels.cache.get(Log)
     const logEmbed = new MessageEmbed()
 
     switch (type) {
 
         case 'newTicket': {
-
             logEmbed.setColor('BLURPLE')
-            logEmbed.setTitle('تیکت جدیدی ایجاد شد')
-            logEmbed.setDescription(`
-            کاربر : ${user.tag} | ${user.id}
-            تیکت : <#ticket_${user.id}>
-            `)
-            logEmbed.setTimestamp()
-            return console.log(`${user.username} just created a ticket on the server ${guild.name}`);
+            logEmbed.setTitle('📬️ | تیکت جدیدی ایجاد شد')
+            logEmbed.setDescription('اطلاعات :\n\nکاربر :`' + user.tag + '` | `' + user.id + '`\nتیکت : `ticket-' + user.id + '`\nتاریخ : `' + new Date().toLocaleString() + '`')
+            return channel.send({ embeds: [logEmbed] })
         }
 
         case 'closeTicket': {
-
             logEmbed.setColor('BLURPLE')
-            logEmbed.setTitle('')
-            logEmbed.setAuthor({ name: '', iconURL: '', url: '' })
-            logEmbed.setDescription('')
-            logEmbed.setTimestamp()
-            logEmbed.setFooter({ text: '', iconURL: '' });
-            return console.log(`${user.username} just closed a ticket on the server ${guild.name}`);
+            logEmbed.setTitle('🔗 | تیکت درحال بسته شدن است')
+            logEmbed.setDescription('اطلاعات :\n\nکاربر :`' + user.tag + '` | `' + user.id + '`\nتیکت : `ticket-' + user.id + '`\nتاریخ : `' + new Date().toLocaleString() + '`')
+            return channel.send({ embeds: [logEmbed] })
         }
 
         case 'reopenTicket': {
-
             logEmbed.setColor('BLURPLE')
-            logEmbed.setTitle('')
-            logEmbed.setAuthor({ name: '', iconURL: '', url: '' })
-            logEmbed.setDescription('')
-            logEmbed.setTimestamp()
-            logEmbed.setFooter({ text: '', iconURL: '' });
-            return console.log(`${user.username} just reopened a ticket on the server ${guild.name}`);
+            logEmbed.setTitle('📬️ | تیکت مجددا باز شد ')
+            logEmbed.setDescription('اطلاعات :\n\nکاربر :`' + user.tag + '` | `' + user.id + '`\nتیکت : `ticket-' + user.id + '`\nتاریخ : `' + new Date().toLocaleString() + '`')
+            return channel.send({ embeds: [logEmbed] })
         }
 
         case 'deleteTicket': {
 
             logEmbed.setColor('BLURPLE')
-            logEmbed.setTitle('')
-            logEmbed.setAuthor({ name: '', iconURL: '', url: '' })
-            logEmbed.setDescription('')
-            logEmbed.setTimestamp()
-            logEmbed.setFooter({ text: '', iconURL: '' });
-            return console.log(`${user.username} just deleted a ticket on the server ${guild.name}`);
+            logEmbed.setTitle('🗑️ | تیکت با موفقیت بسته شد')
+            logEmbed.setDescription('اطلاعات :\n\nکاربر :`' + user.tag + '` | `' + user.id + '`\nتیکت : `ticket-' + user.id + '`\nتاریخ : `' + new Date().toLocaleString() + '`')
+            return channel.send({ embeds: [logEmbed] })
         }
 
         case 'saveTicket': {
-
             logEmbed.setColor('BLURPLE')
-            logEmbed.setTitle('')
-            logEmbed.setAuthor({ name: '', iconURL: '', url: '' })
-            logEmbed.setDescription('')
-            logEmbed.setTimestamp()
+            logEmbed.setTitle('📥️ | محتوای تیکت سیو شد')
+            logEmbed.setDescription('اطلاعات :\n\nکاربر :`' + user.tag + '` | `' + user.id + '`\nتیکت : `ticket-' + user.id + '`\nتاریخ : `' + new Date().toLocaleString() + '`')
             logEmbed.setFooter({ text: '', iconURL: '' });
-            return console.log(`${user.username} just saved a ticket on the server ${guild.name}`);
+            return channel.send({ embeds: [logEmbed] })
         }
     }
 };
