fe7a9ef9c5a5e7c6642af56692fd914570f45103
hadi <65118892+hadiazt@users.noreply.github.com>
2022-04-29 11:59:31 +0430

    Update setup.js

--- commands/setup.js
+++ commands/setup.js
@@ -4,8 +4,8 @@ module.exports = {
     name: 'setup',
 
     execute(client, message) {
-        if (!message.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
-            return message.reply('برای استفاده از این دستور باید مجوز **ADMINISTRATOR** 🚫را داشته باشید');
+        if (!message.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR) || !message.member.permissions.has(Permissions.FLAGS.MANAGE_GUILD)) {
+            return message.reply('برای استفاده از این دستور باید مجوز **MANAGE_GUILD || ADMINISTRATOR** 🚫را داشته باشید');
         }
 
         const setupEmbed = new MessageEmbed();
