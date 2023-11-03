e553c7492efef55de6e20cb467d33e1a07f90803
Mhmd_.akh <79569464+Mhmd-Akh@users.noreply.github.com>
2023-11-03 16:25:42 +0330

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     Client: {
-        Token: "ODMxMDkzNDM0OTM2NxxxxxxxxxxxxxxxxxxxxxxxxxxxxWlq3DVSYPw0oi3nD0TEXsY",
+        Token: "ODMxMDkzNDM0OTM2NzIxNDU5.GAVXJE.9KfwFoFl_qzjIvPNgKIMUyjYFOTa77B9tfFxHA",
         prefix: "t!",
     },
     Channels: {
@@ -26,6 +26,18 @@ module.exports = {
             "1151600949748564028", // Prime
             "1168633292204023838", // Cia
             "1151600958690828449", // Minister
+        ],
+        Moderator: [
+            "1151609030603706499", // Cardinal
+            "1153372394476544081", // Root
+            "1151600128822616145", // Marshall
+            "1151600949748564028", // Prime
+            "1151600958690828449", // Minister
+        ],
+        Jug: [
+            "1168633277666566224", // Your Honer
+            "1168633292204023838", // Cia
+            "1168633307567763487" // Arbiter
         ],
         GetAdmin: "1151602163634675812", // Orgenizer
         Banned: "1151602784567828520",
